d342eb85d6ae568a6a859e2f0ca088b5dc789e18
PigeonCoder1679 <97274758+PigeonCoder1679@users.noreply.github.com>
2022-01-29 19:11:31 +0530

    Add files via upload

---
+++ bg_color_changer.js
@@ -0,0 +1,47 @@
+function change_colorto_red(){
+    document.getElementById("mybody").style.backgroundColor = "red";
+}
+
+function change_colorto_black(){
+    document.getElementById("mybody").style.backgroundColor = "black";
+}
+
+function change_colorto_yellow(){
+    document.getElementById("mybody").style.backgroundColor = "yellow";
+}
+
+function change_colorto_white(){
+    document.getElementById("mybody").style.backgroundColor = "white";
+}
+
+function change_colorto_blue(){
+    document.getElementById("mybody").style.backgroundColor = "blue";
+}
+
+function change_colorto_cyan(){
+    document.getElementById("mybody").style.backgroundColor = "cyan";
+}
+
+function change_colorto_lime(){
+    document.getElementById("mybody").style.backgroundColor = "lime";
+}
+
+function change_colorto_pink(){
+    document.getElementById("mybody").style.backgroundColor = "pink";
+}
+
+function change_colorto_orange(){
+    document.getElementById("mybody").style.backgroundColor = "orange";
+}
+
+function change_colorto_magneta(){
+    document.getElementById("mybody").style.backgroundColor = "magenta";
+}
+
+function change_colorto_silver(){
+    document.getElementById("mybody").style.backgroundColor = "silver";
+}
+
+function change_colorto_gold(){
+    document.getElementById("mybody").style.backgroundColor = "gold";
+}
